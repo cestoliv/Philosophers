e36bd033abb7e8e846127cc1a1b9f07ebe932a24
Olivier Cartier <me@cestoliv.com>
2022-03-14 11:10:50 +0100

    add sem_close

--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -6,15 +6,18 @@
 /*   By: ocartier <ocartier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 12:24:42 by ocartier          #+#    #+#             */
-/*   Updated: 2022/03/08 09:08:41 by ocartier         ###   ########.fr       */
+/*   Updated: 2022/03/14 11:10:15 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosophers.h"
 
-int	free_all(t_phil*philos, int exit_code)
+int	free_all(t_phil *philos, t_params *params, int exit_code)
 {
 	free(philos);
+	sem_close(params->sem_forks);
+	sem_close(params->sem_console);
+	sem_close(params->finished);
 	return (exit_code);
 }
 
@@ -32,6 +35,6 @@ int	main(int argc, char **argv)
 	if (!create_process(&philos, &params))
 		return_code = stop_process(&philos, &params);
 	if (!wait_process(&philos, &params))
-		return (free_all(philos, EXIT_FAILURE));
-	return (free_all(philos, return_code));
+		return (free_all(philos, &params, EXIT_FAILURE));
+	return (free_all(philos, &params, return_code));
 }
